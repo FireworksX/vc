21a93ac08148628c6c460ce7c8364474565254d0
arturabeltins <abeltinsh.a@icloud.com>
2020-04-29 19:42:36 +0300

    make div

---
+++ src/components/Div/Div.tsx
@@ -0,0 +1,15 @@
+import Vue, { VNode } from 'vue'
+import getClassName from '@/helpers/getClassName'
+import './Div.sass'
+
+export default Vue.extend({
+    name: 'vc-Div',
+    computed: {
+        classNames(): any {
+            return getClassName('vc-Div')
+        },
+    },
+    render(h: any) {
+        return <div class={this.classNames()}>{this.$slots.default}</div>
+    },
+})
