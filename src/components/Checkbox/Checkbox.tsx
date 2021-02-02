0f8c389c5d97ad81c01eaed107343b656593d7a7
Aksimka <mks_comp97@mail.ru>
2021-02-02 22:24:57 +0300

    checkbox styles

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,10 +1,16 @@
 import Vue, { VNode } from 'vue'
 import getClassName from '@/helpers/getClassName'
+import VcIcon24CheckBoxOn from '@fireworksx/vc-icons/dist/20/check_box_on.vue'
+import VcIcon24CheckBoxOff from '@fireworksx/vc-icons/dist/20/check_box_off.vue'
 
 export default Vue.extend({
     name: 'vc-Checkbox',
     props: {
         value: { type: Boolean, required: true },
+    },
+    components: {
+        'vc-icon-checkbox-on': VcIcon24CheckBoxOn,
+        'vc-icon-checkbox-off': VcIcon24CheckBoxOff,
     },
     computed: {
         classNames(): any {
@@ -27,7 +33,12 @@ export default Vue.extend({
                     onChange={this.onChange}
                 />
                 <div class="vc-Checkbox__container">
-                    <div class="vc-Checkbox__icon"></div>
+                    <div class="vc-Checkbox__icon vc-Checkbox__icon--on">
+                        <vc-icon-checkbox-on />
+                    </div>
+                    <div class="vc-Checkbox__icon vc-Checkbox__icon--off">
+                        <vc-icon-checkbox-off />
+                    </div>
                     <div class="vc-Checkbox__content">{this.$slots.default}</div>
                 </div>
             </label>
